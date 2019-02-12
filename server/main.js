f4864fe45d481df360efec60926687ad18c10f61
Dr Soze Sakamoto <guachesuede@gmail.com>
2019-02-12 23:16:28 +0800

    update

--- server/main.js
+++ server/main.js
@@ -1,7 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var GrovePi = require('node-grovepi').GrovePi
+var Commands = GrovePi.commands
+var Board = GrovePi.board
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+var board = new Board({
+    debug: true,
+    onError: function(err) {
+        console.log('Something wrong just happened')
+        console.log(err)
+    },
+    onInit: function(res) {
+        if (res) {
+            console.log('GrovePi Version :: ' + board.version())
+
+            var lightSensor = new LightAnalogSensor(2)
+            console.log('Light Analog Sensor (start watch)')
+            lightSensor.on('change', function(res) {
+                console.log('Light onChange value=' + res)
+            })
+            lightSensor.watch()
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+// const fastify = require('fastify')({
+//     logger: true
+// })
+//
+// // Declare a route
+// fastify.get('/', (request, reply) => {
+//     reply.send({ hello: 'world' })
+// })
+//
+// // Run the server!
+// fastify.listen(3000, (err, address) => {
+//     if (err) throw err
+//     fastify.log.info(`server listening on ${address}`)
+// })
